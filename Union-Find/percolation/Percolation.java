bd044e08cef9d7c5c1ae4bf3ff4a809219af9e2a
Paul Huang <paul_hzq@hotmail.com>
2019-05-26 15:04:17 -0400

    modify Percolation to fix backwash bug

--- Union-Find/percolation/Percolation.java
+++ Union-Find/percolation/Percolation.java
@@ -1,54 +1,61 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
-class Percolation {
-	private int ROW;
-	private int COL;
-	private WeightedQuickUnionUF uf;
+public class Percolation {
+    private final int len; 
+	private final WeightedQuickUnionUF uf;
+	private final WeightedQuickUnionUF full;
 	private boolean[] checkOpen;
 	private int countOpenSites;
 
 	public Percolation(int n) {
-		ROW = n;
-		COL = n;
+		if (n <= 0) throw new java.lang.IllegalArgumentException("Index out of bound.");
+		len = n;
 		countOpenSites = 0;
-		checkOpen = new boolean[n*n];
-		uf = new WeightedQuickUnionUF(n*n);
+		checkOpen = new boolean[n*n+2];
+		checkOpen[0] = true;
+		checkOpen[n*n+1] = true;
+		uf = new WeightedQuickUnionUF(n*n+2);
+		full = new WeightedQuickUnionUF(n*n+1);
 	}
 
 	public void open(int row, int col) {
-		validate(row,col);
-		int cell = index(row,col);
-		if(checkOpen[cell]) return;
+		validate(row, col);
+		int cell = index(row, col);
+		if (checkOpen[cell]) return;
 		checkOpen[cell] = true;
 		countOpenSites++;
-		if(row!=1 && isOpen(row-1,col)) {
-			uf.union(index(row-1,col),cell);
-		} else if(row==1) {
-			uf.union(cell,0);
+		if (row != 1 && isOpen(row-1, col)) {
+			uf.union(index(row-1, col), cell);
+			full.union(index(row-1, col), cell);
+		} else if (row == 1) {
+			uf.union(cell, 0);
+			full.union(cell,0);
 		}
-		if(row!=ROW && isOpen(row+1,col)) {
-			uf.union(index(row+1,col),cell);
-		} else if(row==ROW) {
-			uf.union(cell,ROW*COL-1);
+		if (row != len && isOpen(row+1, col)) {
+			uf.union(index(row+1, col), cell);
+			full.union(index(row+1, col), cell);
+		} else if (row == len) {
+			uf.union(cell, len*len+1);
 		}
-		if(col!=1 && isOpen(row,col-1)) {
-			uf.union(index(row,col-1),cell);
+		if (col != 1 && isOpen(row, col-1)) {
+			uf.union(index(row, col-1), cell);
+			full.union(index(row, col-1), cell);
 		}
-		if(col!=COL && isOpen(row,col+1)) {
-			uf.union(index(row,col+1),cell);
+		if (col != len && isOpen(row, col+1)) {
+			uf.union(index(row, col+1), cell);
+			full.union(index(row, col+1), cell);
 		}
 	}
 
 	public boolean isOpen(int row, int col) {
-		validate(row,col);
-		return checkOpen[index(row,col)];
+		validate(row, col);
+		return checkOpen[index(row, col)];
 	}
 
 	public boolean isFull(int row, int col) {
-		validate(row,col);
-		if(index(row,col)==0) return isOpen(row,col);
-		return uf.connected(index(row,col),0);
+		validate(row, col);
+		return full.connected(index(row, col), 0);
 	}
 
 	public int numberOfOpenSites() {
@@ -56,20 +63,17 @@ class Percolation {
 	}
 
 	public boolean percolates() {
-		return uf.connected(0,ROW*COL-1);
+		return uf.connected(0, len*len+1);
 	}
 
-	private void validate(int row,int col) {
-		if(row<=0 || row>ROW || col<=0 || col>COL) {
-			throw new java.lang.IndexOutOfBoundsException("Index out of bound.");
+	private void validate(int row, int col) {
+		if (row <= 0 || row > len || col <= 0 || col > len) {
+			throw new java.lang.IllegalArgumentException("Index out of bound.");
 		}
 	}
 
 	private int index(int row, int col) {
-		return (row-1) * COL + col - 1;
+		return (row-1) * len + col; 
 	}
 
-	public static void main(String[] args) {
-
-	}
 }
--- Union-Find/percolation/PercolationStats.java
+++ Union-Find/percolation/PercolationStats.java
@@ -3,19 +3,23 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
 
 
-class PercolationStats {
-	private double[] results;
-	private int n;
-	private int trials;
+public class PercolationStats {
+	private final double[] results;
+	private final int n;
+	private final int trials;
+	private final double mean;
+	private final double stddev;
 
 	public PercolationStats(int n, int trials) {
-		if(n<=0 || trials<=0) throw new java.lang.IllegalArgumentException();
+		if (n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException();
 		this.n = n;
 		this.trials = trials;
 		results = new double[trials];
-		for(int i=0;i<trials;i++) {
+		for (int i = 0; i < trials; i++) {
 			results[i] = percolPercentage();
 		}
+		this.mean = mean();
+		this.stddev = stddev();
 	}
 	public double mean() {
 		return StdStats.mean(results);
@@ -24,29 +28,29 @@ class PercolationStats {
 		return StdStats.stddev(results);
 	}
 	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(trials);
+		return mean - 1.96 * stddev / Math.sqrt(trials);
 	}
 	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(trials);
+		return mean + 1.96 * stddev / Math.sqrt(trials);
 	}
 
 	private double percolPercentage() {
 		Percolation perc = new Percolation(n);
-		while(!perc.percolates()) {
-			int i=StdRandom.uniform(1,n+1);
-			int j=StdRandom.uniform(1,n+1);
-			if(perc.isOpen(i,j)) continue;
-			perc.open(i,j);
+		while (!perc.percolates()) {
+			int i = StdRandom.uniform(1, n+1);
+			int j = StdRandom.uniform(1, n+1);
+			if (perc.isOpen(i, j)) continue;
+			perc.open(i, j);
 		}
 		double square = n*n;
 		return perc.numberOfOpenSites()/square;
 	}
 	public static void main(String[] args) {
-		int N = Integer.parseInt(args[0]);
-        int T = Integer.parseInt(args[1]);
-        PercolationStats ps = new PercolationStats(N, T);
-        StdOut.printf("mean                    = %f%n", ps.mean());
-        StdOut.printf("stddev                  = %f%n", ps.stddev());
+		int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, t);
+        StdOut.printf("mean                    = %f%n", ps.mean);
+        StdOut.printf("stddev                  = %f%n", ps.stddev);
         StdOut.printf("95%% confidence interval = [%f, %f%n] ", ps.confidenceLo(), ps.confidenceHi());
 	}
 }
